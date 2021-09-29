2e0f7d36bc09405e7c4c0b49e408b5d4f717c4a0
Seifbarouni <seif.barouni11@gmail.com>
2021-09-29 10:47:22 +0100

    initial commit

---
+++ pages/components/Container.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+
+interface ContainerProps {
+  children: any
+}
+
+export const Container: React.FC<ContainerProps> = ({ children }) => {
+  const [selected, setSelected] = useState('home')
+  const [dark, setDark] = useState('dark')
+  return (
+    <div className="pt-12 flex flex-col">
+      <nav className="p-6 flex  lg:px-72 md:px-12 justify-between  items-center w-full">
+        <div
+          className="bg-gray-800 p-2 rounded-xl hover:bg-gray-700 cursor-pointer active:transform  active:scale-110 transition duration-150  ease-out"
+          onClick={() => setDark(`${dark === 'dark' ? 'light' : 'dark'}`)}
+        >
+          {dark === 'dark' ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 fill-current text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 fill-current text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            </svg>
+          )}
+        </div>
+        <div className="text-white flex space-x-8">
+          <span
+            className={`${
+              selected === 'home' && 'border-b pb-1'
+            } cursor-pointer`}
+            onClick={() => setSelected('home')}
+          >
+            Home
+          </span>
+          <span
+            className={`${
+              selected === 'projects' && 'border-b pb-1'
+            } cursor-pointer`}
+            onClick={() => setSelected('projects')}
+          >
+            Projects
+          </span>
+          <span
+            className={`${
+              selected === 'timeline' && 'border-b pb-1'
+            } cursor-pointer`}
+            onClick={() => setSelected('timeline')}
+          >
+            Timeline
+          </span>
+        </div>
+      </nav>
+      <main className="flex flex-col justify-center px-8">
+        {children}
+        <footer></footer>
+      </main>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+import { Container } from './components/Container'
+
+export default function Home() {
+  return (
+    <Container>
+      <Head>
+        <title>Seif Barouni</title>
+      </Head>
+      <div className="flex flex-col xl:px-96 lg:px-72 md:px-16 justify-center items-start">
+        <h1 className="text-3xl md:text-5xl text-white font-semibold pt-6">
+          Hey, I'm Seif Barouni
+        </h1>
+        <h2 className="text-gray-400 pt-4 ml-0.5 md:w-11/12 mb-16">
+          I'm a computer science student and a web developer. I love creating
+          new things with technology. I'm currently learning mobile development
+          using
+          <a
+            className="underline text-blue-500 ml-1"
+            target="_blank"
+            href="https://reactnative.dev/"
+          >
+            React Native.
+          </a>
+        </h2>
+        <h1 className="text-3xl md:text-4xl text-white font-semibold">
+          My recent projects
+        </h1>
+      </div>
+    </Container>
+  )
+}
