3da7a6331d5a22f64299bb3848348f2f4ba23b12
Seifbarouni <seif.barouni11@gmail.com>
2022-01-19 15:52:21 +0100

    deleted stars

--- components/home/Project.tsx
+++ components/home/Project.tsx
@@ -1,7 +1,6 @@
 interface ProjectProps {
   link: string
   title: string
-  stars: number
   description: string
   learned: string[]
 }
@@ -9,7 +8,6 @@ interface ProjectProps {
 const Project: React.FC<ProjectProps> = ({
   link,
   title,
-  stars,
   description,
   learned,
 }) => {
@@ -19,22 +17,9 @@ const Project: React.FC<ProjectProps> = ({
       target="_blank"
       className="border-2 dark:border-gray-700 border-gray-300  rounded-md p-3  cursor-pointer flex flex-col space-y-2 shadow-sm"
     >
-      <div className="flex items-center justify-between">
-        <h1 className="dark:text-white text-black text-lg font-semibold">
-          {title}
-        </h1>
-        <span className="flex items-center space-x-1 text-blue-500 dark:text-blue-400">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-          </svg>
-          <span>{stars}</span>
-        </span>
-      </div>
+      <h1 className="dark:text-white text-black text-lg font-semibold">
+        {title}
+      </h1>
       <p className="dark:text-gray-400 text-gray-600 text-sm">{description}</p>
       <div>
         <span className="dark:text-white text-black font-semibold">
--- components/home/RecentProjects.tsx
+++ components/home/RecentProjects.tsx
@@ -9,35 +9,30 @@ const RecentProjects: React.FC<RecentProjectsProps> = () => {
         link="https://github.com/Seifbarouni/go-create"
         title="go-create"
         description="A tool that helps me create my apps (console apps, back-end web apps, full-stack web apps)."
-        stars={0}
         learned={['Golang']}
       />
       <Project
         link="https://github.com/Seifbarouni/BraveBlog-frontend"
         title="Brave Blog"
         description="A blog built with React that has a simple livestream feature."
-        stars={0}
         learned={['React', 'Typescript', 'PostgreSQL', 'Tailwind']}
       />
       <Project
         link="https://github.com/Seifbarouni/Project-Management-Web-App"
         title="Project Management Web App"
         description="A Spring Boot application that helps teams and individuals organize their projects. "
-        stars={1}
         learned={['Java', 'Spring Boot', 'VanillaJS', 'PostreSQL', 'Thymeleaf']}
       />
       <Project
         link="https://github.com/Seifbarouni/Comm"
         title="Comm"
         description="A social media app inspired by Reddit."
-        stars={0}
         learned={['Svelte', 'PostgreSQL', 'Tailwind']}
       />
       <Project
         link="https://github.com/Seifbarouni/svelte-covid19-tracker"
         title="Covid-19 Tracker"
         description="An app that displays news about covid-19 from covid19api.com."
-        stars={0}
         learned={['Svelte', 'Tailwind']}
       />
     </div>
