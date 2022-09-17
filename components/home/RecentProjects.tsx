a3eebddc7ab0648998b1ba681fa779176a19853d
Seifbarouni <seif.barouni11@gmail.com>
2022-09-17 09:53:59 +0100

    add new project

--- components/home/RecentProjects.tsx
+++ components/home/RecentProjects.tsx
@@ -6,6 +6,12 @@ const RecentProjects: React.FC<RecentProjectsProps> = () => {
   return (
     <div className="flex flex-col space-y-3 mb-16" id="projects">
       <Project
+        link="https://github.com/Seifbarouni/seifbarouni.live"
+        title="seifbarouni.live"
+        description="My portfolio website whith a blog feature."
+        used={['Next.js', 'Tailwind CSS', 'TypeScript']}
+      />
+      <Project
         link="https://github.com/Seifbarouni/go-create"
         title="go-create"
         description="A tool that helps me create my apps (console apps, back-end web apps, full-stack web apps)."
